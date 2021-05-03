8839a15d77339f299482757fcce37862340d809e
Robbert Tuerlings <robberttg@protonmail.com>
2021-05-03 20:40:04 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,42 +59,6 @@ module.exports = {
 				output: '/robots.txt',
 			},
     },
-    {
-			resolve: `gatsby-plugin-sitemap`,
-			options: {
-				output: `/sitemap.xml`,
-				// Exclude specific pages or groups of pages using glob parameters
-				// See: https://github.com/isaacs/minimatch
-				// The example below will exclude the single `path/to/page` and all routes beginning with `category`
-				exclude: [`/category/*`, `/path/to/page`],
-				query: `
-					{
-						site {
-							siteMetadata {
-								siteUrl
-							}
-						}
-
-						allSitePage {
-							nodes {
-								path
-							}
-						}
-				}`,
-				resolveSiteUrl: ({ site, allSitePage }) => {
-					//Alternatively, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
-					return site.siteMetadata.siteUrl;
-				},
-				serialize: ({ site, allSitePage }) =>
-					allSitePage.nodes.map((node) => {
-						return {
-							url: `${site.siteMetadata.siteUrl}${node.path}`,
-							changefreq: `daily`,
-							priority: 0.7,
-						};
-					}),
-			},
-		},
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
