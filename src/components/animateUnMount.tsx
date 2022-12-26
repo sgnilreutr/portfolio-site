4c2a14b71cd6d01608dd28d0d8e840845e9d9a57
Robbert <robberttg@protonmail.com>
2022-12-26 11:43:56 +0100

    Update animateUnMount.tsx

--- src/components/animateUnMount.tsx
+++ src/components/animateUnMount.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react'
 
 const AnimatedMountUnmount = ({ children }: { children: ReactNode }) => (
-  <div className="animate-fadeInUp">{children}</div>
+  <div className="motion-safe:animate-fadeInUp">{children}</div>
 )
 
 export default AnimatedMountUnmount
